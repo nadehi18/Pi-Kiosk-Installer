9c8b908812b62bb19908fee9463f3e7f8c0d858f
BuildTools <unconfigured@null.spigotmc.org>
2019-07-07 16:16:18 -0400

    Fixed more issues with scripts.

--- scripts/enable-autostart.sh
+++ scripts/enable-autostart.sh
@@ -6,3 +6,5 @@ echo "[Desktop Entry]" > $1
 echo "Type=Application" >> $1
 echo "Name=Kiosk" >> $1
 echo "Exec=chromium-browser --kiosk $2" >> $1
+
+exit
--- scripts/enable-refresh.sh
+++ scripts/enable-refresh.sh
@@ -6,3 +6,5 @@ echo "#!/bin/bash" > $1
 echo "DISPLAY=:0 xdotool getactivewindow key F5" >> $1
 
 chmod +x $1
+
+exit
